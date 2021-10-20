0b56f3d5533ef22a114e9a3f6f0da8f012fcfac7
Alex <Adavisson@eagles.ewu.edu>
2021-10-20 16:38:43 -0700

    Cleaned up code, added start screen

--- script.js
+++ script.js
@@ -36,13 +36,13 @@ Quote and Emoji
 // blockquote
 const quote = document.querySelector("blockquote");
 
-const author = document.querySelector("p.author");
+const author = document.querySelector("span.author");
 // emoji
 const emoji = document.querySelector("p");
 // background
 const bgImage = document.querySelector("body");
 
-bgImage.style.backgroundImage = "url(/assets/mountain.jpg)";
+bgImage.style.backgroundImage = "url(/assets/cat1.jpg)";
 
 /*
  * Navigate to Items with Event Listeners
@@ -57,9 +57,9 @@ const prevButton = document.querySelector("img.prev");
 // get random button
 const randButton = document.querySelector("img.random");
 
-// listen for clicks on next button
-nextButton.addEventListener("click", function () {
-	// update, then increase item number with each click
+
+function nextItem(){
+	
 	itemNumber += 1;
 
 	// if at LAST item
@@ -72,82 +72,71 @@ nextButton.addEventListener("click", function () {
 	quote.innerHTML = data[itemNumber].quote;
 	emoji.innerHTML = data[itemNumber].emoji;
 	bgImage.style.backgroundImage = data[itemNumber].image;
-});
+	author.innerHTML ="- "+ data[itemNumber].author;
+	audio.src=data[itemNumber].audio;
+        audio.play();
+}
+
+function prevItem(){
+
+	
+// update, then DEcrease item number with each click
+itemNumber -= 1;
+
+// if at FIRST item
+if (itemNumber < 0) {
+	// reset counting to last item
+	itemNumber = data.length - 1;
+}
+
+// display data based on array index
+quote.innerHTML = data[itemNumber].quote;
+emoji.innerHTML = data[itemNumber].emoji;
+bgImage.style.backgroundImage = data[itemNumber].image;
+author.innerHTML = "- " + data[itemNumber].author;
+audio.src=data[itemNumber].audio;
+	audio.play();
+}
+
+function randItem(){
+// load in button styling
+	document.querySelector('img.next').src="assets/next.svg";
+	document.querySelector('img.prev').src="assets/prev.svg";
+	document.querySelector('img.random').src="assets/random.svg";
+
+// generate random number, for total number, of items (data)
+randomNumber = Math.floor(Math.random() * data.length);
+
+// display data based random number
+quote.innerHTML = data[randomNumber].quote;
+emoji.innerHTML = data[randomNumber].emoji;
+bgImage.style.backgroundImage = data[randomNumber].image;
+author.innerHTML = "- "+data[randomNumber].author;
+audio.src=data[randomNumber].audio;
+	audio.play();
+}
+// listen for clicks on next button
+nextButton.addEventListener("click", nextItem);
 
 // listen for clicks on previous button
-prevButton.addEventListener("click", function () {
-	// update, then DEcrease item number with each click
-	itemNumber -= 1;
-
-	// if at FIRST item
-	if (itemNumber < 0) {
-		// reset counting to last item
-		itemNumber = data.length - 1;
-	}
-
-	// display data based on array index
-	quote.innerHTML = data[itemNumber].quote;
-	emoji.innerHTML = data[itemNumber].emoji;
-	bgImage.style.backgroundImage = data[itemNumber].image;
-});
+prevButton.addEventListener("click", prevItem);
 
 // listen for clicks on random button
-randButton.addEventListener("click", function () {
-	// generate random number, for total number, of items (data)
-	randomNumber = Math.floor(Math.random() * data.length);
-
-	// display data based random number
-	quote.innerHTML = data[randomNumber].quote;
-	emoji.innerHTML = data[randomNumber].emoji;
-	bgImage.style.backgroundImage = data[randomNumber].image;
-});
+randButton.addEventListener("click", randItem);
 
 // listen for keypress next
 document.addEventListener("keyup", function () {
 	if (event.keyCode === 39) {
-		// update, then increase item number with each click
-		itemNumber += 1;
-
-		// if at LAST item
-		if (itemNumber > data.length - 1) {
-			// reset counting to first item
-			itemNumber = 0;
-		}
-
-		// display data based on array index
-		quote.innerHTML = data[itemNumber].quote;
-		emoji.innerHTML = data[itemNumber].emoji;
-		bgImage.style.backgroundImage = data[itemNumber].image;
+		nextItem();
 	}
 
 	if (event.keyCode === 37) {
-		// update, then DEcrease item number with each click
-		itemNumber -= 1;
-
-		// if at FIRST item
-		if (itemNumber < 0) {
-			// reset counting to last item
-			itemNumber = data.length - 1;
-		}
-
-		// display data based on array index
-		quote.innerHTML = data[itemNumber].quote;
-		emoji.innerHTML = data[itemNumber].emoji;
-		bgImage.style.backgroundImage = data[itemNumber].image;
+		prevItem();
 	}
 
 	//spacebar
 	if (event.keyCode === 32) {
-		// generate random number, for total number, of items (data)
-		randomNumber = Math.floor(Math.random() * data.length);
-
-		// display data based random number
-		quote.innerHTML = data[randomNumber].quote;
-		emoji.innerHTML = data[randomNumber].emoji;
-		bgImage.style.backgroundImage = data[randomNumber].image;
-		var audio = document.getElementById("audio");
-		audio.src=data[randomNumber].audio;
-        audio.play();
+		randItem();
 	}
 	
 });
